0ef3997fbf23856f74ccc174546d5565623d19ae
Abhishek Kumar <abhishekkumar880247@gmail.com>
2026-04-09 22:39:10 +0530

    Adding baseendpoint for Railway deployment - new Api endpoint

--- public/config.js
+++ public/config.js
@@ -3,6 +3,7 @@
 // Change API_BASE_URL here to point to your Spring Boot backend
 
 // Set the Spring Boot backend base URL here
-const API_BASE_URL = 'http://localhost:8080';
+// const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://revaris-backend-production.up.railway.app';
 
 export { API_BASE_URL };
